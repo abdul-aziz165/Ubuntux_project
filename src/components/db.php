a9909428bd7abeedb95e68c9e4496b9197b480ad
abdul-aziz165 <abdulazizransali@gmail.com>
2025-03-15 13:32:28 +0000

    Added PHP file

--- src/components/db.php
+++ src/components/db.php
@@ -1,9 +1,14 @@
-$servername = "sqlXXX.infinityfree.com"; // Replace with your InfinityFree database hostname
-$username = "your_db_username";  // Replace with your InfinityFree database username
-$password = "your_db_password";  // Replace with your InfinityFree database password
-$database = "your_db_name";      // Replace with your InfinityFree database name
+<?php
+$servername = "ubuntuxx.infinityfree.com"; 
+$username = "your_db_username";  
+$password = "your_db_password";  
+$database = "your_db_name";     
 
 $conn = new mysqli($servername, $username, $password, $database);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+else {
+    echo "Connected successfully!";
+}
+?>
